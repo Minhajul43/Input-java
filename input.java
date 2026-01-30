9b18d6eb4406c7ab29338fc50ece32413456b359
Minhajul43 <minhajul255685islam@gmail.com>
2026-01-31 00:50:44 +0600

    input Integer

---
+++ input.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class input {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter you age:");
+    int age = input.nextInt();
+    
+    System.out.println("you age:" +age);
+   
+  }
+
+}
+
+// import java.util.Scanner;
+
+// public class InputInt {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);  // Create Scanner object
+        
+//         System.out.print("Enter your age: ");
+//         int age = input.nextInt();  // Read integer from user
+        
+//         System.out.println("Your age is: " + age);
+//     }
+// }
